46dd58c656dcce99eb138069b4982b05f3977115
Marissa vonWolff <marissa.wolff@gmx.de>
2023-09-04 16:52:33 +0200

    validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :projects
+  validates :name, presence: true
+  validates :title, presence: true
+  validates :company, presence: true
 end
