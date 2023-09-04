58bd3bddbe5535015bbe476387339982d131f9da
Bijan Sabbagh <bijans@gmail.com>
2023-09-04 19:10:47 +0200

    Project view page created

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,13 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
-  resources :projects do
-    resources :
+  skip_before_action :authenticate_user!, only: [ :home, :about_us ]
+
   def home
+  end
+
+  def show
+  raise
+  @project = Project.find(params[:id])
+  @user = current_user
+
   end
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,7 @@
 class ProjectsController < ApplicationController
+  skip_before_action :authenticate_user!
+
+  def show
+
+  end
 end
