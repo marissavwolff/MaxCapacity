f86e7baf1a9f6905553ddbcdaa27625381ebfa79
Marissa vonWolff <marissa.wolff@gmx.de>
2023-09-05 22:27:16 +0200

    members_view

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,6 +2,5 @@ class Member < ApplicationRecord
   has_many :projects, through: :project_members
   validates :name, presence: true
   validates :title, presence: true
-  validates :capacity, presence: true
   has_one_attached :photo
 end
