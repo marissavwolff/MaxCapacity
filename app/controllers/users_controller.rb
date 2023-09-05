1d704a5df3ee6e3a0e67b61bb2d1640c1bb257cb
Marissa vonWolff <marissa.wolff@gmx.de>
2023-09-05 12:26:12 +0200

    Add dotenv-rails

--- app/controllers/users_controller.rb
+++
@@ -1,4 +0,0 @@
-class UsersController < ApplicationController
-  def user_params
-    params.require(:user).permit(:name, :email, :phone_number, :password, :password_confirmation, :company, :title)
-end
