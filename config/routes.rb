fcc80c8e5d02dfc5ad120c74de3d3a63a062847e
Charlotte Reyburn <charlottereyburn7@gmail.com>
2023-09-06 16:27:37 +0100

    homepage

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
   get 'demo', to: 'pages#demo', as: :demo
   get 'pricing', to: 'pages#pricing', as: :pricing
   get 'contact_us', to: 'pages#contact_us', as: :contact_us
-  get 'sign_up', to: 'registrations#new', as: :new_user_registration
+  # get 'sign_up', to: 'registrations#new', as: :new_user_registration
 
 end
