077b525baac1906e82ac3737e34e8fd208ce6e59
Bijan Sabbagh <bijans@gmail.com>
2023-09-11 15:18:02 +0200

    fixed an issue on new project page

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -1,4 +1,4 @@
 class Tool < ApplicationRecord
   belongs_to :project
-  enum :app, { jira: 0, asana: 1, google_meets: 2, slack: 3 }
+  enum :app, { jira: 0, asana: 1, google_meets: 2, Trello: 3 }
 end
