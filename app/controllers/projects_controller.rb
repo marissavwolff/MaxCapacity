29a72c12d30128902cc604842f280f61c0d76318
Georgia Bean-Butler <georgia.bean@icloud.com>
2023-09-12 11:46:21 +0100

    changes to project milestone tracker

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,3 +1,5 @@
+require 'asana'
+
 class ProjectsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_project, only: [:show, :edit, :update, :destroy]
@@ -26,7 +28,6 @@ class ProjectsController < ApplicationController
     @project = Project.find(params[:id])
     asannn = '1/1205422980318130:72fecd4355e1f6dfe789e2b414108a98'
 
-    require 'asana'
     client = Asana::Client.new do |c|
       c.authentication :access_token, asannn
     end
@@ -89,7 +90,6 @@ class ProjectsController < ApplicationController
 
     asannn = '1/1205422980318130:72fecd4355e1f6dfe789e2b414108a98'
 
-    require 'asana'
     client = Asana::Client.new do |c|
         c.authentication :access_token, asannn
     end
@@ -103,7 +103,10 @@ class ProjectsController < ApplicationController
     @asana_hours = 0
     @result.each do |task|
       task.tags.each do |tag|
-        @asana_hours += tag.name.to_i
+        if tag.name == "1" || tag.name == "3" || tag.name == "5" || tag.name == "7"
+          hour = tag.name.to_i
+          @asana_hours += hour
+        end
       end
     end
 
@@ -122,8 +125,34 @@ class ProjectsController < ApplicationController
   end
 
   def update
-    @project.update(project_params)
-    add_members
+    if params[:project][:asana_update].present?
+      asannn = '1/1205422980318130:72fecd4355e1f6dfe789e2b414108a98'
+
+      client = Asana::Client.new do |c|
+          c.authentication :access_token, asannn
+      end
+      workspaces = client.workspaces.get_workspaces(options: {pretty: true})
+      workspace_id = workspaces.to_a[0].gid
+      @goals = client.goals.get_goals(workspace: workspace_id, options: {pretty: true})
+      # first_goal = goals.to_a[0]
+      @result = client.tasks.get_tasks_for_project(project_gid: '1205422662236262', options: {pretty: true, fields: ["name", "due_on", "completed", "assignee.name", "start_on", "tags.name", "notes", "projects.name"]})
+      @result = @result.elements
+
+      @asana_hours = 0
+      @result.each do |task|
+        task.tags.each do |tag|
+          if tag.name == "1" || tag.name == "3" || tag.name == "5" || tag.name == "7"
+            hour = tag.name.to_i
+            @asana_hours += hour
+          end
+        end
+      end
+
+      @project.update(asana_hours: @asana_hours)
+    else
+      @project.update(project_params)
+      add_members
+    end
     redirect_to project_path
   end
 
